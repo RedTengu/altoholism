d7ed3f4d3b411bc6a253cb23637eba63f37198de
Red Yakuza <muratborokov@gmail.com>
2023-07-31 17:47:09 +0300

    feat: сверстать header.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className='header'>
+      <ul className='header__lang-list'>
+        <li>
+          <button className='header__lang-btn header__lang-btn_type_active' type='button' aria-label='Переключиться на русский язык'>
+            Ru
+          </button>
+        </li>
+        <li>
+          <button className='header__lang-btn' type='button' aria-label='Переключиться на английский язык'>
+            En
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
