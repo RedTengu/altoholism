5b55eb6199b68e4dbe03ca1a3d3a4b95c7e33a44
Red Yakuza <muratborokov@gmail.com>
2023-08-14 14:17:09 +0300

    feat: добавить временный лого.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import './Header.css';
-
-// добавить лого
+import logo from '../../images/logo.png'
 
 function Header() {
   return (
     <header className='header'>
+      <a className='logo-link' href="#"><img className='logo' src={logo} alt='Логотип Альтоголизм' /></a>
       <ul className='header__lang-list'>
         <li>
           <button className='header__lang-btn header__lang-btn_type_active' type='button' aria-label='Переключиться на русский язык'>
