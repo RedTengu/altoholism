0484be4e02f15030855a8d49d07660a6088aa04d
Red Yakuza <muratborokov@gmail.com>
2023-08-20 14:01:57 +0300

    feat: добавить router и компонент 404.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import MainMenu from '../MainMenu/MainMenu';
+import NotFound from '../NotFound/NotFound';
 
 import './Main.css';
 
 function Main() {
   return (
     <main className='content'>
-      <MainMenu />
+      <Routes>
+        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<MainMenu />} />
+      </Routes>
     </main>
   );
 }
